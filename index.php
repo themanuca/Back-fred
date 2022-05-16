6cf80f1ebcc30f0981cad75741df73dcf30cfcb6
themanuca <ssergioca@gmail.com>
2022-05-16 14:36:07 -0300

    Find da lista ok

---
+++ index.php
@@ -0,0 +1,46 @@
+<?php
+include_once "conexao.php";
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <title>CRUD - PHP FETCH</title>
+</head>
+
+<body>
+    <div class="container">
+        <div class="row mt-4">
+            <div class="col-lg-12 d-flex justify-content-between aling-items-center">
+                <div>
+                    <h4>ESTOQUE</h4>
+                </div>
+
+                <div>
+                    <button type="button" class="btn btn-outline-primary" data-bs-toggle="modal" data-bs-target="#cadProdutoModal">
+                       Cadastrar
+                    </button>
+
+                </div>
+            </div>
+        </div>
+        <hr>
+        <div class="row">
+            <div class="col-lg-12">
+
+                <span class="listar-produto"></span>
+
+
+
+            </div>
+        </div>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    <script src="js/custom.js"></script>
+</body>
+
+</html>
---
+++ js/custom.js
@@ -0,0 +1,9 @@
+const tbody = document.querySelector(".listar-produto");
+
+const listarUsuarios = async (pagina) => {
+    const dados = await fetch("./list.php?pagina=" + pagina);
+    const resposta = await dados.text();
+    tbody.innerHTML = resposta;
+}
+
+listarUsuarios(1);
